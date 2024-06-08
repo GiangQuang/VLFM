9cd06666764b5c30548153a4343eae1b320055c6
GiangQuang <116422976+GiangQuang@users.noreply.github.com>
2024-06-08 17:48:53 +0700

    Device Assignment FE done

--- admin/config/defaultSettings.ts
+++ admin/config/defaultSettings.ts
@@ -17,7 +17,7 @@ const Settings: ProLayoutProps & {
   colorWeak: false,
   title: 'Văn Lang University',
   pwa: true,
-  logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
+  logo: 'https://jmp.sh/s/DmqiDvpNmQY9GoEh1EQe',
   iconfontUrl: '',
   token: {
     // 参见ts声明，demo 见文档，通过token 修改样式
